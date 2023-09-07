aca2cf21ef6d71e655215eb1e5813eccd55e516c
Charlotte Kies <chopekies@gmail.com>
2023-09-07 08:58:55 -0400

    Wrote unit tests for unique pairs

--- whiteboarding/src/test/java/com/beginner/ArraysAndListsTest.java
+++ whiteboarding/src/test/java/com/beginner/ArraysAndListsTest.java
@@ -5,15 +5,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ArraysAndListsTest {
 
-    private com.beginner.ArraysAndLists getSums;
+    private com.beginner.ArraysAndLists arraysAndLists;
 
     @BeforeEach
     void setUp() {
-        getSums = new com.beginner.ArraysAndLists();
+        arraysAndLists = new com.beginner.ArraysAndLists();
     }
 
     @AfterEach
@@ -25,7 +23,7 @@ class ArraysAndListsTest {
     @Test
     void test_Get_Sums_Returns_Target() {
         int[] array = {7,3,2};
-        int[] actual = getSums.getSums(array, 9);
+        int[] actual = arraysAndLists.getSums(array, 9);
         int[] expected = {7,2};
 
         Assert.assertEquals("Given array: [7,3,2] and target: 9, indexes [0,2] add up to target",expected[0], actual[0]);
@@ -36,7 +34,7 @@ class ArraysAndListsTest {
     @Test
     void test_Get_Sums_When_Target_Is_0() {
         int[] array = {7,3,2};
-        int[] actual = getSums.getSums(array, 0);
+        int[] actual = arraysAndLists.getSums(array, 0);
         int[] expected = new int[0];
 
         Assert.assertEquals("Given array: [7,3,2] and target: 0, expect an empty array because none of the given values add up to target.", actual.length, expected.length);
@@ -50,7 +48,7 @@ class ArraysAndListsTest {
     @Test
     void test_Get_Sums_When_Target_Is_Negative_And_Array_Only_Positive() {
         int[] array = {7,3,2};
-        int[] actual = getSums.getSums(array, -9);
+        int[] actual = arraysAndLists.getSums(array, -9);
         int[] expected = new int[0];
 
         Assert.assertEquals("Given array: [7,3,2] and target: 0, expect an empty array because none of the given values add up to the negative target.", actual.length, expected.length);
@@ -60,7 +58,7 @@ class ArraysAndListsTest {
     @Test
     void test_Get_Sums_When_Array_Has_Negative_Numbers_And_Target_Is_Positive() {
         int[] array = {-7,-3,-2};
-        int[] actual = getSums.getSums(array, 9);
+        int[] actual = arraysAndLists.getSums(array, 9);
         int[] expected = new int[0];
 
         Assert.assertEquals("Given array: [7,3,2] and target: 0, expect an empty array because none of the given values add up to the negative target.", actual.length, expected.length);
@@ -69,7 +67,7 @@ class ArraysAndListsTest {
     @Test
     void test_Get_Sums_When_Array_Has_Two_Possible_Solutions() {
         int[] array = {1, 8, 3, 6};
-        int[] actual = getSums.getSums(array, 9);
+        int[] actual = arraysAndLists.getSums(array, 9);
         int[] expected = new int[2];
         expected[0] = 1;
         expected[1] = 8;
@@ -81,10 +79,33 @@ class ArraysAndListsTest {
     }
 
     /** Unique Pairs Tests **/
+
+    // Helper methods
+    private boolean containsSameElements(int[] array1, int[] array2) {
+        if (array1.length != array2.length) {
+            return false;
+        }
+        for (int value : array1) {
+            if (!containsValue(array2, value)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    private boolean containsValue(int[] array, int value) {
+        for (int element : array) {
+            if (element == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // tests
     @Test
     void test_unique_pairs_returns_expected() {
         int[] array = {1, 8, 3, 6};
-        int[][] actual = getSums.getUniquePairs(array, 9);
+        int[][] actual = arraysAndLists.getUniquePairs(array, 9);
         int[] firstPair = {1,8};
         int[] secondPair = {3,6};
         int[][] expected = {firstPair, secondPair};
@@ -96,26 +117,120 @@ class ArraysAndListsTest {
         Assert.assertEquals("Given array: [1, 8, 3, 6] and target: 9, expect a new array: [[1, 8], [3, 6]",  expected[0][0], actual[0][0]);
     }
 
+    @Test
+    void test_unique_pairs_when_nums_is_empty() {
+        int[] array = new int[0];
+        int[][] actual = arraysAndLists.getUniquePairs(array, 9);
+        int[] firstPair = new int[0];
+        int[] secondPair = new int[0];
+        int[][] expected = {firstPair, secondPair};
+        int expectedRowLength = 0;
 
-//    Empty Input Array Test: Test the case when the input array nums is empty. The method should return an empty two-dimensional array.
+        Assert.assertTrue("", actual.length == 0);
+        Assert.assertEquals(".", expectedRowLength, actual[0].length);
+        Assert.assertEquals("",  expected[0][0], actual[0][0]);
+    }
 
-//    Input Array with No Pairs Test: Test the case when there are no pairs of elements in the input array that add up to the target. The method should return an empty two-dimensional array.
+    @Test
+    void test_unique_pairs_when_multiple_pairs_match() {
+        int[] array = {1,2,3,4,5,6,7,8,9,8,7,6,5,4,3,2,1};
+        int[][] actual = arraysAndLists.getUniquePairs(array, 9);
+        int[] firstPair = {1,8};
+        int[] secondPair = {2,7};
+        int[] thirdPair = {4,5};
+        int[][] expected = {firstPair, secondPair, thirdPair};
+        int expectedRowLength = 2;
+        // TODO: Consider the possibility that a solution might not add values to rows in the same order, and that rows might not be added in the same order
+        // TODO: Consider validating that there is a row with 1 and 8 in it, for example
+        Assert.assertTrue("The number of rows (number of unique pairs) should be 3", actual.length == expected.length);
+        Assert.assertEquals("The rows should each contain 2 values.", expectedRowLength, actual[0].length);
 
-//    Target Sum Negative Test: Test a scenario where the target sum is negative. Verify that the method correctly handles this case and returns expected unique pairs.
+        Assert.assertEquals("Row 0 column 0 should be 1",  expected[0][0], actual[0][0]);
+        Assert.assertEquals("Row 0 column 1 should be 8",  expected[0][1], actual[0][1]);
+        Assert.assertEquals("row 1 column 1 should be 7",  expected[1][1], actual[1][1]);
+        Assert.assertEquals("row 1 column 0 should be 2",  expected[1][0], actual[1][0]);
+        Assert.assertEquals("row 2 column 1 should be 5",  expected[2][1], actual[2][1]);
+        Assert.assertEquals("row 2 column 0 should be 4",  expected[2][0], actual[2][0]);
+    }
 
-//    Target Sum Negative No Pairs Test: Test a scenario where the target sum is negative. Verify that the method correctly handles this case and returns expected unique pairs.
+    @Test
+    void test_unique_pairs_when_target_is_negative() {
+        int[] array = {1,2,3,4,5,-7,6,5,4,3,-2,1};
+        int[][] actual = arraysAndLists.getUniquePairs(array, -9);
+        int[] firstPair = {-7,-2};
+        int[][] expected = {firstPair};
+        int expectedRowLength = 2;
 
-//    Negative Numbers Test: Test the method with an input array containing negative numbers and a target sum that includes both positive and negative numbers.
+        Assert.assertTrue("The number of rows (number of unique pairs) should be 1", actual.length == expected.length);
+        Assert.assertEquals("The rows should each contain 2 values.", expectedRowLength, actual[0].length);
 
-//    Multiple Unique Pairs Test: Test a scenario where there are multiple pairs of elements in the input array that add up to the target. Verify that the method returns all the unique pairs.
+        Assert.assertTrue(containsSameElements(expected[0], actual[0]));
+    }
 
-//    Duplicate Pairs Test: Test a scenario where there are duplicate pairs of elements in the input array that add up to the target. Ensure that the method returns only unique pairs.
+    void test_unique_pairs_returns_expected_when_target_negative() {
+        int[] array = {-1, 3, -8, 6};
+        int[][] actual = arraysAndLists.getUniquePairs(array, -9);
+        int[] firstPair = {1,8};
+        int[][] expected = {firstPair};
+        int expectedRowLength = 2;
 
-//    Large Input Test: Test the method with a large input array and target sum to ensure it performs efficiently and doesn't exceed time or space limits.
+        Assert.assertTrue("Number of rows in should be greater than 0. There should be one row, representing the single pair of values.", actual.length == 1);
+        Assert.assertEquals("There should be 1 row, of length 2. Each row represents a pair of values that add up to the target.", expectedRowLength, actual[0].length);
+        Assert.assertTrue("Given array: [-1, 3, -8, 6] and target: -9, expect a new array: [[-1, -8]]] or [[-8, -1]]", containsSameElements(expected[0], actual[0]));
+    }
 
-//    Target Sum Zero Test: Test a scenario where the target sum is zero. Consider testing when the array contains pairs of 0s and pairs of negative / positive numbers that add up to 0..
+    void test_unique_pairs_returns_empty_array_when_no_pairs_match() {
+        int[] array = {1, 2, 3, 4, 12, 9};
+        int[][] actual = arraysAndLists.getUniquePairs(array, -9);
+        int[][] expected = {{}};
+        int expectedRowNumber = 0;
 
-//    Edge Case Test / Invalid Input Test(s): Test the method with edge cases, such as very small input arrays or arrays with only one element.
+        Assert.assertTrue("Number of rows in should be 0.", actual.length == expectedRowNumber);
+    }
+
+
+    void test_unique_pairs_returns_one_pair_when_duplicate_pairs_exist() {
+        int[] array = {1, 2, 2, 1, 1, 2};
+        int[][] actual = arraysAndLists.getUniquePairs(array, 3);
+        int[] firstPair = {1,2};
+        int[][] expected = {firstPair};
+        int expectedRowLength = 2;
+
+        Assert.assertTrue("Number of rows in should be greater than 0. There should be one row, representing the single pair of values.", actual.length == 1);
+        Assert.assertEquals("There should be 1 row, of length 2. Each row represents a pair of values that add up to the target.", expectedRowLength, actual[0].length);
+        Assert.assertTrue("Given array: [1, 2, 2, 1, 1, 2] and target: 3, expect a new array: [[2, 1]]] or [[1, 2]]", containsSameElements(expected[0], actual[0]));
+    }
+
+    void test_unique_pairs_returns_expected_when_target_is_zero() {
+        int[] array = {0, 0, -1, -1};
+        int[][] actual = arraysAndLists.getUniquePairs(array, 0);
+        int[] firstPair = {0,0};
+        int[] secondPair = {-1,-1};
+        int[][] expected = {firstPair, secondPair};
+        int expectedRowLength = 2;
+        int expectedNumberRows = 2;
+
+        Assert.assertTrue("Number of rows in should be 2. There should be two rows, each representing the single pair of values.", actual.length == expectedNumberRows);
+        Assert.assertEquals("There should be 2 rows, of length 2. Each row represents a pair of values that add up to the target.", expectedRowLength, actual[0].length);
+        Assert.assertEquals("There should be 2 rows, of length 2. Each row represents a pair of values that add up to the target.", expectedRowLength, actual[1].length);
+        Assert.assertTrue("Given array: [0, 0, -1, -1] and target: 0, expect a new array: [[2, 1]]] or [[1, 2]]", containsSameElements(expected[0], actual[0]));
+        Assert.assertTrue("Given array: [0, 0, -1, -1] and target: 0, expect a new array: [[2, 1]]] or [[1, 2]]", containsSameElements(expected[1], actual[1]));
+    }
+
+
+    void test_unique_pairs_when_input_invalid() {
+        int[] array = {0};
+        int[][] actual = arraysAndLists.getUniquePairs(array, 0);
+        int[][] expected = {{}};
+        int expectedRowLength = 0;
+        int expectedNumberRows = 0;
+        Assert.assertTrue("Number of rows in should be 0.", actual.length == expectedNumberRows);
+
+        int[] emptyArray = new int[0];
+        actual = arraysAndLists.getUniquePairs(emptyArray,5);
+        Assert.assertTrue("Number of rows in should be 0.", actual.length == expectedNumberRows);
+
+    }
 
 //    Performance Test: If your method needs to handle large input arrays efficiently, consider writing a performance test to ensure it meets the required performance criteria.
 
